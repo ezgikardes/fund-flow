89bc4e3ff3c25d414cebe57583029f35b497797e
ezgikardes <ezgikardes@gmail.com>
2025-01-09 21:09:11 +0100

    Add service layer for Account

---
+++ src/main/java/github/ezgikardes/fundflow/service/AccountService.java
@@ -0,0 +1,11 @@
+package github.ezgikardes.fundflow.service;
+
+import github.ezgikardes.fundflow.model.Account;
+
+import java.util.List;
+
+public interface AccountService {
+    public List<Account> getAccountsByUser(Long userId);
+
+    public Account createAccount(Account account);
+}
---
+++ src/main/java/github/ezgikardes/fundflow/service/AccountServiceImpl.java
@@ -0,0 +1,38 @@
+package github.ezgikardes.fundflow.service;
+
+import github.ezgikardes.fundflow.model.Account;
+import github.ezgikardes.fundflow.model.User;
+import github.ezgikardes.fundflow.repository.AccountRepository;
+import github.ezgikardes.fundflow.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AccountServiceImpl implements AccountService{
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public List<Account> getAccountsByUser(Long userId) {
+        if (userId == null) {
+            throw new IllegalArgumentException("User ID cannot be null");
+        }
+        return accountRepository.findByUserId(userId);
+    }
+
+    @Override
+    public Account createAccount(Account account) {
+        User user = userRepository.findById(account.getUser().getId()).orElse(null);
+        if(user != null){
+          account.setUser(user);
+          return accountRepository.save(account);
+        }
+        return null;
+    }
+}
