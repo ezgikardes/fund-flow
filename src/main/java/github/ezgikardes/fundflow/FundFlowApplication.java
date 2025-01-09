c461825c64804644ffe08d51348bf94bf3d0f0c9
ezgikardes <ezgikardes@gmail.com>
2025-01-09 16:41:42 +0100

    this is the first commit

---
+++ src/main/java/github/ezgikardes/fundflow/FundFlowApplication.java
@@ -0,0 +1,13 @@
+package github.ezgikardes.fundflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/ezgikardes/fundflow/FundFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package github.ezgikardes.fundflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
