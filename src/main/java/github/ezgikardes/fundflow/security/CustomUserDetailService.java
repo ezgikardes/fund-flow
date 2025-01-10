b325a2d5360acf4e00aad0f5968b445dbc41e8e1
ezgikardes <ezgikardes@gmail.com>
2025-01-10 18:50:55 +0100

    security layer updated

---
+++ src/main/java/github/ezgikardes/fundflow/security/CustomUserDetailService.java
@@ -0,0 +1,27 @@
+package github.ezgikardes.fundflow.security;
+
+import github.ezgikardes.fundflow.model.User;
+import github.ezgikardes.fundflow.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles("ROLE_" + user.getRole().name()) // Kullanıcının rolü (CUSTOMER veya ADMIN)
+                .build();
+    }
+}
