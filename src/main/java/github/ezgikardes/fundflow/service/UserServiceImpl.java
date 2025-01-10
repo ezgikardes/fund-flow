d34d144c8e4afdda85805bc46f476d4de6ce53d5
ezgikardes <ezgikardes@gmail.com>
2025-01-10 18:50:13 +0100

    Add password encoder to registerUser

--- src/main/java/github/ezgikardes/fundflow/service/UserServiceImpl.java
+++ src/main/java/github/ezgikardes/fundflow/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package github.ezgikardes.fundflow.service;
 import github.ezgikardes.fundflow.model.User;
 import github.ezgikardes.fundflow.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +15,9 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public User getUserById(Long id) {
         return userRepository.findById(id).orElse(null);
@@ -21,6 +25,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
